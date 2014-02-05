f75ece019d377169afa2ca7c0d9fb9917a6cf86d
akun1012 <yangzongkun@gmail.com>
2014-02-05 14:20:32 -0700

    part1

--- Minibase/src/global/GlobalConst.java
+++ Minibase/src/global/GlobalConst.java
@@ -3,15 +3,29 @@ package global;
 public interface GlobalConst {
 
   public static final int MINIBASE_MAXARRSIZE = 50;
-  public static final int NUMBUF = 50;
+  /**
+   * Zongkun
+   */
+  //public static final int NUMBUF = 50;
+  public static final int NUMBUF = 4000;
 
   /** Size of page. */
-  public static final int MINIBASE_PAGESIZE = 1024;           // in bytes
-
+  /**
+   * Zongkun
+   */
+  //public static final int MINIBASE_PAGESIZE = 1024;           // in bytes
+  public static final int MINIBASE_PAGESIZE = 4096;
   /** Size of each frame. */
-  public static final int MINIBASE_BUFFER_POOL_SIZE = 1024;   // in Frames
-
-  public static final int MAX_SPACE = 1024;   // in Frames
+  /**
+   * Zongkun
+   */
+  //public static final int MINIBASE_BUFFER_POOL_SIZE = 1024;
+  public static final int MINIBASE_BUFFER_POOL_SIZE = 4096;   // in Frames
+  /**
+   * Zongkun
+   */
+  //public static final int MAX_SPACE = 1024;   // in Frames
+  public static final int MAX_SPACE = 4096;
   
   /**
    * in Pages => the DBMS Manager tells the DB how much disk 
